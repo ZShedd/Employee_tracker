1cc2cdc9c33f9b7af33aae9a6d7d2ae7f59c9c17
Clown Supreme <zachary.h.shedd@gmail.com>
2023-03-08 12:57:31 -0800

    Some Bug fixes

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,13 +1,13 @@
 INSERT INTO departments (department_id, department_name)
 VALUES (001, "Administration"),
        (002, "Human Resources"),
-       (003, "Research and Development"),
+       (003, "Development"),
        (004, "Information Technology");
 
 INSERT INTO roles (role_id, title, salary, department_id)
 VALUES (001, "CEO", 250000, 001),
        (002, "Manager", 150000, 001),
-       (003, "HR Consultant", 120000, 002),
+       (003, "HR Manager", 120000, 002),
        (004, "Developer", 90000, 003),
        (005, "IT Technician", 100000, 004);
 
